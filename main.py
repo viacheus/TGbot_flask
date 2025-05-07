17703b2f8904da975258a812099a146e231d6222
Viacheus <glorianfox@gmail.com>
2025-05-07 21:59:48 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# Импортируем необходимые классы.
+import logging
+from telegram.ext import Application, MessageHandler, filters
+from config import BOT_TOKEN
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
+async def echo(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    await update.message.reply_text(update.message.text)
+
+
+def main():
+    # Создаём объект Application.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    # Создаём обработчик сообщений типа filters.TEXT
+    # из описанной выше асинхронной функции echo()
+    # После регистрации обработчика в приложении
+    # эта асинхронная функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(filters.TEXT, echo)
+
+    # Регистрируем обработчик в приложении.
+    application.add_handler(text_handler)
+
+    # Запускаем приложение.
+    application.run_polling()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
